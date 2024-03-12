eb01bfab3fe062087000ff5cd7c2e9969fc9a12c
Julieanna Bacon <julieannabacon2014@gmail.com>
2024-03-12 15:01:54 -0400

    preprocess: Support centering netcdf crop window arbitrarily and added additional documentation to config file and docstrings. Added project to grid function

--- icedyno/preprocess/crop.py
+++ icedyno/preprocess/crop.py
@@ -9,6 +9,7 @@ import os
 import pathlib
 
 import luigi
+import numpy as np
 import xarray as xr
 
 
@@ -16,13 +17,17 @@ class CropFiles(luigi.Task):
     """
     Crop IMS and MASIE NetCDF files from the center of their grids (where x, y == 1/2*sie.shape) based on input window_size.
 
+    Supports centering the window of the cropped files at different locations.
+    See config/preprocess_netcdf.toml for configuration settings.
     """
 
     input_dir = luigi.Parameter()
     output_dir = luigi.Parameter()
 
+    center_coordinates = luigi.Parameter()
+
     window_size = luigi.IntParameter(default=4000)
-    year = luigi.IntParameter()
+    year = luigi.IntParameter()  # Determined at runtime
 
     def output(self) -> luigi.LocalTarget:
         return luigi.LocalTarget(
@@ -39,19 +44,35 @@ class CropFiles(luigi.Task):
         )
 
         for cdf_filepath in input_cdf_files:
+            # Set output file name and check if the output file already exists on disk.
             output_filename = (
                 os.path.join(year_output_dir, pathlib.Path(cdf_filepath).stem)
-                + f"_grid{self.window_size}.nc"
+                + f"_grid{self.window_size}"
             )
+            if self.center_coordinates != "None":
+                output_filename += (
+                    f"_{self.center_coordinates.replace(' ', '')}center.nc"
+                )
+            else:
+                output_filename += ".nc"
+
             if os.path.exists(output_filename):
                 print(cdf_filepath, "already on disk, skipping...")
 
             # Open the original NetCDF file
             ds = xr.open_dataset(cdf_filepath, engine="h5netcdf")
 
-            x_coord = ds["x"].shape[0] // 2
-            y_coord = ds["y"].shape[0] // 2
-            window = self.window_size * 1000
+            if self.center_coordinates != "None":
+                # Expects self.center_coordinates x,y to be "x, y" float values.
+                x, y = [float(coord) for coord in self.center_coordinates.split(",")]
+
+                # Project x, y to the defined grid and find the index in the grid corresponding to that pair.
+                x_coord = find_closest_index_in_grid(x, ds.x)
+                y_coord = find_closest_index_in_grid(y, ds.y)
+            else:
+                x_coord = ds["x"].shape[0] // 2
+                y_coord = ds["y"].shape[0] // 2
+            window = self.window_size * 1000  # from km to meters
 
             cropped_ds = ds.sel(
                 x=slice(x_coord - window, x_coord + window),
@@ -60,6 +81,28 @@ class CropFiles(luigi.Task):
 
             # Write the cropped data to a new NetCDF file
             cropped_ds.to_netcdf(output_filename, engine="h5netcdf")
+
+
+def find_closest_index_in_grid(target: float, coordinates: np.array) -> int:
+    """
+    Given a target coordinate in projected coordinates (x or y) and the list of coordinates, return the index of the closest number in the coordinates.
+    Assumes a 1km grid.
+    """
+    assert np.allclose(coordinates % 1000, 500)
+    start = coordinates[
+        0
+    ]  # Assume that the first element of coordinates is the minimum number in the list
+    # -12287500.0 for IMS 1km data.
+
+    # Define the step size
+    grid_resolution = 1000  # meters
+
+    # Calculate the index of the closest number
+    index = int((target - start) // grid_resolution)
+
+    # If the solution is correct, the target and the found value should never be more than the grid_resolution apart.
+    assert abs(coordinates[index] - target) < grid_resolution
+    return index
 
 
 if __name__ == "__main__":
@@ -74,7 +117,7 @@ if __name__ == "__main__":
 
     ## Change acording to your number of cores
     n_workers = 10
-    years = range(2014, 2025)
+    years = range(2015, 2025)
 
     tasks = [CropFiles(year=year) for year in years]
     luigi.build(tasks, workers=n_workers, local_scheduler=True)
