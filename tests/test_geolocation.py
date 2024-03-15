6b3883e7a636e8719505df1d2001e59bb2b76645
Julieanna Bacon <julieannabacon2014@gmail.com>
2024-03-14 21:13:40 -0400

    bugfix: original function does not undo itself, contrary to what written tests suggests.

--- tests/test_geolocation.py
+++ tests/test_geolocation.py
@@ -5,12 +5,14 @@ from icedyno.preprocess import geolocation
 
 def test_xy_to_latlon_inversion():
     """Test that xy to latlon and latlon to xy invert each other"""
-    lat = 60  # -77.508333
-    lon = -80  # 164.754167
+    lat = -77.508333
+    lon = 164.754167
 
     xy_converted_coordinates = geolocation.polar_lonlat_to_xy(
         longitude=lon, latitude=lat
     )
-    reverted_lat_lon = geolocation.polar_xy_to_latlon(*xy_converted_coordinates)
 
+    print(xy_converted_coordinates)
+    reverted_lat_lon = geolocation.polar_xy_to_lonlat(*xy_converted_coordinates)
+    print(reverted_lat_lon)
     assert np.allclose((lon, lat), reverted_lat_lon)
