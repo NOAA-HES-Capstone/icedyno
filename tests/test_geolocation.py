30709d7cdd2e6d26f00b4fb9e580c3c370612969
Julieanna Bacon <julieannabacon2014@gmail.com>
2024-03-16 22:24:52 -0400

    tests, cleanup: Updated tests with more ranges. Cleaned up misc commented out print statements

--- tests/test_geolocation.py
+++ tests/test_geolocation.py
@@ -5,12 +5,15 @@ from icedyno.preprocess import geolocation
 
 def test_xy_to_latlon_inversion():
     """Test that xy to latlon and latlon to xy invert each other"""
-    lat = 77.508333
-    lon = -164.754167
+    lats = [77.508333, 80, 67, 55]
+    lons = [-164.754167, -80, 90, 155]
 
-    xy_converted_coordinates = geolocation.polar_lonlat_to_xy(
-        longitude=lon, latitude=lat
-    )
+    for i in range(len(lats)):
+        lat = lats[i]
+        lon = lons[i]
+        xy_converted_coordinates = geolocation.polar_lonlat_to_xy(
+            longitude=lon, latitude=lat
+        )
 
-    reverted_lat_lon = geolocation.polar_xy_to_lonlat(*xy_converted_coordinates)
-    assert np.allclose((lon, lat), reverted_lat_lon)
+        reverted_lat_lon = geolocation.polar_xy_to_lonlat(*xy_converted_coordinates)
+        assert np.allclose((lon, lat), reverted_lat_lon)
