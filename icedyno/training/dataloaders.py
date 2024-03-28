f618d26377106ec5a6ca7bb31b95300708a65bda
Julieanna Bacon <julieannabacon2014@gmail.com>
2024-03-28 18:03:28 -0400

    Bugfix training: Fixed major bug in data loader with extracting date and years associated with specific files

--- icedyno/training/dataloaders.py
+++ icedyno/training/dataloaders.py
@@ -4,8 +4,19 @@ import numpy as np
 import tensorflow as tf
 import xarray as xr
 
+## Not ideal, but sets the relative data path and the dataset to use
+WINDOW_SIZE = 2000  # km
+try:
+    IN_COLAB = True
+except ModuleNotFoundError:
+    IN_COLAB = False
+data_root = "ims_netcdf_1km_cropped_2_000km_window_74lat_-170lon/"
+if not IN_COLAB:
+    data_root = os.path.join("..", "data", data_root)
+##################################################################
 
-def load_sie_data(year: int, day: int, data_root: str, WINDOW_SIZE: int) -> np.array:
+
+def load_sie_data(year: int, day: int) -> np.array:
     """Returns a 2D numpy array copy of the IMS surface values"""
     return (
         load_nc_file(year, day, data_root, WINDOW_SIZE)
@@ -14,12 +25,9 @@ def load_sie_data(year: int, day: int, data_root: str, WINDOW_SIZE: int) -> np.a
     )
 
 
-def load_target_sie_data(
-    year: int, day: int, data_root: str, WINDOW_SIZE: int
-) -> np.array:
+def load_binary_sie_data(year, day) -> np.array:
     """Returns a 2D numpy array copy of the IMS surface values"""
-    ds = load_nc_file(year, day, data_root, WINDOW_SIZE)
-    sie = ds.IMS_Surface_Values[0].values.copy()
+    sie = load_sie_data(year, day)
     binary_sie = sie.copy()
     binary_sie[sie != 3] = 0
 
@@ -28,26 +36,26 @@ def load_target_sie_data(
     return binary_sie
 
 
-def binarize_data(sie: np.array) -> np.array:
+def trinarize_data(sie: np.array) -> np.array:
     """
     New SIE:
     0: Open water/out of bounds
     1: Sea ice or lake ice (lake mask not applied)
     2: Land
     """
-    binary_sie = sie.copy()
-    binary_sie[sie != 3] = 0
+    trinary_sie = sie.copy()
+    trinary_sie[sie != 3] = 0
 
     # Sea and Lake Ice is treated as 1
-    binary_sie[sie == 3] = 1
+    trinary_sie[sie == 3] = 1
 
     # Land and Snow-Covered Land is sent to 2.
-    binary_sie[sie == 2] = 2
-    binary_sie[sie == 4] = 2
-    return binary_sie
+    trinary_sie[sie == 2] = 2
+    trinary_sie[sie == 4] = 2
+    return trinary_sie
 
 
-def load_nc_file(year: int, day: int, data_root: str, WINDOW_SIZE: int) -> xr.Dataset:
+def load_nc_file(year: int, day: int) -> xr.Dataset:
     """
     Loads the cropped, grid-corrected netcdf files on the Beaufort Sea with 74,0lat_-170,0lon
     Loads a single .nc file for a given year and day
@@ -64,12 +72,11 @@ def load_nc_file(year: int, day: int, data_root: str, WINDOW_SIZE: int) -> xr.Da
         return dataset
 
 
-class BinaryTargetNetCDFGenerator(tf.keras.utils.Sequence):
+class BinaryTargetGenerator(tf.keras.utils.Sequence):
     """
     Generator for Keras training to allow multiprocessing and training on batches with only the
     batch itself being loaded into memory.
 
-    Targets a binary image (ice/no ice).
     """
 
     def __init__(
@@ -77,16 +84,20 @@ class BinaryTargetNetCDFGenerator(tf.keras.utils.Sequence):
         filenames: list[str],
         batch_size: int = 2,
         dim: tuple = (8000, 8000, 5),
-        shuffle: bool = True,
     ):
+        self.batch_size = batch_size
+        self.dim = dim  # (height, width, channel)
+
         self.filenames = sorted(filenames)
         self.years = self.years_from_filenames()
         self.days = self.days_from_filenames()
-        self.batch_size = batch_size
-        self.dim = dim  # (height, width, channel)
-        self.shuffle = True
+
+        # Dictionary to look up where the year day (2022, 151) is in self.years and self.days
+        self.index_of_year_day = {
+            year_day: index for index, year_day in enumerate(zip(self.years, self.days))
+        }
+
         self.data_IDs = self._get_data_ids()
-        self.on_epoch_end()
 
     def years_from_filenames(self):
         years = [
@@ -123,24 +134,31 @@ class BinaryTargetNetCDFGenerator(tf.keras.utils.Sequence):
 
         return X.astype("float16"), y.astype("int32")
 
-    def on_epoch_end(self):
-        """Updates indexes after each epoch"""
-        if self.shuffle:
-            np.random.shuffle(self.data_IDs)
-
-    def load_n_day_chunk(self, i, n):
+    def load_n_day_chunk(self, year, day, n):
         """Starts at year, day and returns the next n days of processed SIE."""
+        i = self.index_of_year_day[(year, day)]
         days = self.days[i : i + n]
         years = self.years[i : i + n]
 
         sie_chunk = []
         for year, day in zip(years, days):
-            sie = binarize_data(load_sie_data(year, day))
+            sie = trinarize_data(load_sie_data(year, day))
             sie_chunk.append(sie)
 
         assert len(sie_chunk) == n
         # Use np.stack to stack the individual 2D arrays along a new third axis, resulting in (height, width, channels)
         return np.stack(sie_chunk, axis=-1)
+
+    def load_binary_y_for_day(self, year, day, n):
+        """If the initial X data day is 10 with offset 3, load day 13, binarize it, and return the shape expected of y data"""
+        i = self.index_of_year_day[(year, day)]
+        y_data = np.expand_dims(
+            load_binary_sie_data(
+                self.years[i + self.dim[2]], self.days[i + self.dim[2]]
+            ),
+            axis=-1,
+        )
+        return y_data
 
     def _data_generation(self, batch_data_ids):
         """Generates data containing batch_size samples"""
@@ -148,14 +166,10 @@ class BinaryTargetNetCDFGenerator(tf.keras.utils.Sequence):
         y = np.empty((self.batch_size, self.dim[0], self.dim[1], 1), dtype="int32")
 
         for i, (year, day) in enumerate(batch_data_ids):
-            # Load a 5-day chunk as the input
-            X[i,] = self.load_n_day_chunk(i, self.dim[2])
+            # Load a n-day chunk as the input
+            X[i] = self.load_n_day_chunk(year, day, self.dim[2])
+
             # Load the next day as the target
-            y[i,] = np.expand_dims(
-                load_target_sie_data(
-                    self.years[i + self.dim[2]], self.days[i + self.dim[2]]
-                ),
-                axis=-1,
-            )
+            y[i] = self.load_binary_y_for_day(year, day, self.dim[2])
 
         return X, y
